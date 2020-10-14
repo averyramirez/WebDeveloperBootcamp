0460f96cd0853605390c35bbc524532f51b1a3d7
averyramirez <averyyramirez@gmail.com>
2020-10-14 16:24:18 -0700

    Else If
    More conditionals

--- Next Project/jsScript.js
+++ Next Project/jsScript.js
@@ -1,5 +1,39 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("your number is less than 0.5")
-    console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("your number is less than 0.5")
+//     console.log(random);
+// }
+
+
+
+
+
+// const dayOfWeek = 'Tuesday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("I hate mondays")
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log("Yay!")
+// } else if (dayOfWeek === 'Friday') {
+//     console.log("Fridays are cool")
+// } else {
+//     console.log("MEH")
+// }
+
+
+
+
+
+
+
+// const age = 70;
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10!")
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20!")
+// } else if (age > 65) {
+//     console.log("You are old. You pay $10!")
+// }
