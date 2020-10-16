c59b6085a2cbf91d31d0f560c46b76e31272a481
averyramirez <averyyramirez@gmail.com>
2020-10-16 09:45:53 -0700

    whileLoops
    using while loops until user guesses the secret code

---
+++ Next Project/whileLoops.js
@@ -0,0 +1,13 @@
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log(count)
+// }
+
+const SECRET = "BabyHippo";
+
+let guess = prompt("Enter the secret code...");
+while (guess !== SECRET) {
+    guess = prompt("Enter the secret code...");
+}
+console.log("Congrats, you got the secret!")
