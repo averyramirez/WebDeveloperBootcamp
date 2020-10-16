77e19c543ff962b3b93eef342aeccff52c98947f
averyramirez <averyyramirez@gmail.com>
2020-10-16 09:53:28 -0700

    The Break Keyword
    using the break keyword in a loop

---
+++ Next Project/breakKeyword.js
@@ -0,0 +1,11 @@
+// let input = prompt("Hey, say something!")
+// while (true) {
+//     input = prompt(input);
+//     if (input === "Stop copying me") break;
+// }
+// console.log("Okay, you win!")
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if (i === 100) break;
+}
