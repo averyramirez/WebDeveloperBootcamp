ce83c23388caced393e6b44bba8b473444871e66
averyramirez <averyyramirez@gmail.com>
2020-10-17 13:55:22 -0700

    Intro to functions
    various functions

---
+++ Next Project/firstFunction.js
@@ -0,0 +1,28 @@
+// function singSong() {
+//     console.log("DO")
+//     console.log("RE")
+//     console.log("MI")
+// }
+
+// function greet(firstName, lastName) {
+//     console.log(`Hey there, ${firstName} ${lastName[0]}.!`)
+// }
+
+// function repeat(str, numTimes) {
+//     let result = '';
+//     for (let i = 0; i < numTimes; i++) {
+//         result += str;
+//     }
+//     console.log(result);
+// }
+
+
+// function add(x, y) {
+//     console.log(x + y)
+// }
+
+function rant(message.toUpperCase) {
+    console.log()
+    console.log()
+    console.log()
+}
