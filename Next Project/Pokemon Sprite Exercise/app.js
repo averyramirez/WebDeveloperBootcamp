20f1397b51e0be5c826f993d6ba27e386b2b77e6
averyramirez <averyyramirez@gmail.com>
2020-11-21 17:43:11 -0800

    Pokemon Sprite
    Exercise including sprites of pokemon

---
+++ Next Project/Pokemon Sprite Exercise/app.js
@@ -0,0 +1,6 @@
+// https://raw.githubusercontent.com/PokemonAPI/sprites/master/pokemon/1.png
+
+const container = document.querySelector('#container');
+const newImg = document.createElement('img');
+newImg.src = 'https://raw.githubusercontent.com/PokemonAPI/sprites/master/pokemon/1.png';
+container.appendChild(newImg);
