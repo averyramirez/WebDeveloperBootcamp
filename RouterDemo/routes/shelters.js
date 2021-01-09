3b565496576fe178dd77192cc7e29dc1e213366d
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-01-09 14:32:08 -0800

    RouterDemo
    Added routers to page

--- RouterDemo/routes/shelters.js
+++ RouterDemo/routes/shelters.js
@@ -4,3 +4,17 @@ const router = express.Router();
 router.get('/shelters', (req, res) => {
     res.send("ALL SHELTERS")
 })
+
+router.post('/shelters', (req, res) => {
+    res.send("CREATING SHELTER")
+})
+
+router.get('/shelters/:id', (req, res) => {
+    res.send("VIEWING ONE SHELTER")
+})
+
+router.get('/shelters/:id/edit', (req, res) => {
+    res.send("EDITING ONE SHELTER")
+})
+
+module.exports = router;
